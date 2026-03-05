2730f2219679f0e1d76d70489aedcb7c0e6c1660
Martyna <margaj366@gmail.com>
2026-03-05 12:56:04 +0100

    hexapawn

--- Projekt1/main.py
+++ Projekt1/main.py
@@ -1,11 +1,11 @@
 from easyAI import TwoPlayerGame
+import random
 
 # Convert D7 to (3,6) and back...
 to_string = lambda move: " ".join(
     ["ABCDEFGHIJ"[move[i][0]] + str(move[i][1] + 1) for i in (0, 1)]
 )
 to_tuple = lambda s: ("ABCDEFGHIJ".index(s[0]), int(s[1:]) - 1)
-
 
 class Hexapawn(TwoPlayerGame):
     """
@@ -24,6 +24,7 @@ class Hexapawn(TwoPlayerGame):
 
         self.players = players
         self.current_player = 1
+        self.removed_pawns =  []
 
     def possible_moves(self):
         moves = []
@@ -45,7 +46,24 @@ class Hexapawn(TwoPlayerGame):
         self.player.pawns[ind] = move[1]
 
         if move[1] in self.opponent.pawns:
+            # owner is the index (0 or 1) of the player who owned the captured pawn
+            owner = 0 if self.opponent is self.players[0] else 1
+            # store (owner, column) so resurrected pawn has the same column as original
+            self.removed_pawns.append((owner, move[1][1]))
             self.opponent.pawns.remove(move[1])
+        
+        if self.removed_pawns and random.random() < 0.3:
+            owner, col = random.choice(self.removed_pawns)
+            # compute owner's home row (player 0 starts at row 0, player 1 at M-1)
+            home_row = 0 if self.players[owner].direction == 1 else (self.size[0] - 1)
+            start_pos = (home_row, col)
+            # per your rule, don't check occupancy here (you said it cannot happen)
+            self.players[owner].pawns.append(start_pos)
+            try:
+                self.removed_pawns.remove((owner, col))
+            except ValueError:
+                pass
+
 
     def lose(self):
         return any([i == self.opponent.goal_line for i, j in self.opponent.pawns]) or (
