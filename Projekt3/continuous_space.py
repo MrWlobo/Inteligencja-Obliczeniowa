405cabfffe949bef197ebc6d8b95cd45f2abe84b
MrWlobo <michaldworniczak40@gmail.com>
2026-04-28 09:49:44 +0200

    Improved gamma comparison

--- Projekt3/continuous_space.py
+++ Projekt3/continuous_space.py
@@ -3,6 +3,7 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
 import matplotlib.pyplot as plt
+import numpy as np
 
 os.makedirs("models", exist_ok=True)
 os.makedirs("logs", exist_ok=True)
@@ -33,7 +34,7 @@ class RewardLogger(BaseCallback):
 
 
 env = gym.make("Pendulum-v1")
-gamma = 0.99
+gamma = 0.10
 
 model = PPO(
     "MlpPolicy",
@@ -53,16 +54,26 @@ model = PPO(
 
 logger = RewardLogger()
 
-model.learn(total_timesteps=300_000, callback=logger)
+model.learn(total_timesteps=200_000, callback=logger)
 
 model.save(f"models/ppo_pendulum_gamma_{gamma}")
 
 rewards = logger.rewards
 
-plt.plot(rewards)
-plt.title("PPO Learning Curve")
+window_size = 20
+
+rewards_smoothed = np.convolve(rewards, np.ones(window_size) / window_size, mode="valid")
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(range(window_size - 1, len(rewards)), rewards_smoothed, color='red', linewidth=2,
+         label=f"Reward")
+
+plt.title(f"PPO Learning Curve (Gamma: {gamma})")
 plt.xlabel("Episode")
 plt.ylabel("Reward")
+plt.legend()
+plt.grid(True, alpha=0.3)
 
 plt.savefig(f"logs/learning_curve_gamma_{gamma}.png", dpi=300)
 plt.show()
